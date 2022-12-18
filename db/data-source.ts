a73b4e7c5d229dcf771247d403abe5652e09c1bd
Muhammad Nafi' Furqon Diani <muhammad.diani@manuva.com>
2022-12-19 02:08:23 +0700

    chore: update data source config for production

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,8 +1,20 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
+// export const dataSourceOptions: DataSourceOptions = {
+//   type: 'sqlite',
+//   database: 'db.sqlite',
+//   entities: ['dist/**/*.entity{.ts,.js}'],
+//   migrations: ['dist/db/migrations/*.js'],
+//   logging: true,
+// };
+
 export const dataSourceOptions: DataSourceOptions = {
-  type: 'sqlite',
-  database: 'db.sqlite',
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/db/migrations/*.js'],
   logging: true,
